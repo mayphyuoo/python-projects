499876b4303eb9e84d355c417f411a3e381faefd
May Oo <mayphyuoo1211@gmail.com>
2022-03-25 00:54:58 +1100

    add documentation and cleanup

--- higher-lower/main.py
+++ higher-lower/main.py
@@ -4,26 +4,36 @@ import random
 import os
 
 
-def clearConsole(): return os.system(
+def clearConsole():
+    """clear out console terminal"""
+    return os.system(
     'cls' if os.name in ('nt', 'dos') else 'clear')
 
+
 # return 0 if pair1 >= pair2 else return 1
-
-
 def calculateAnswer(pair1, pair2, guess, score):
-  if pair1['follower_count'] >= pair2['follower_count']:
-    if guess == "A" or guess == "a":
-      score += 1
-      return True, score
-    else:
-      return False, score
-  else:
-    if guess == "A" or guess == "a":
-      return False, score
-    else:
-      score += 1
-      return True, score
+    """Checks followers against user's guess
+  and returns True and add 1 to score if they got it right.
+  Or False if they got it wrong."""
 
+    if pair1['follower_count'] >= pair2['follower_count']:
+        if guess == "A" or guess == "a":
+            score += 1
+            return True, score
+        else:
+            return False, score
+    else:
+        if guess == "A" or guess == "a":
+            return False, score
+        else:
+            score += 1
+            return True, score
+
+
+def printData(celeb):
+    """input: pair, return fstring: celeb's name, description and country"""
+    # print(f'{celeb['name']}: {celeb["follower_count"]}')
+    return f" {celeb['name']}, a {celeb['description']}, from {celeb['country']}."
 
 # main starts here
 # first random pair
@@ -32,32 +42,34 @@ pair = random.sample(data, 2)
 gameContinue = True
 score = 0
 
+# start game
 while gameContinue:
-  clearConsole()
-  print(logo)
+    clearConsole()
+    print(logo)
 
-  if score > 0:
-    print(f"You're right! Current score: {score}")
+    if score > 0:
+        print(f"You're right! Current score: {score}")
 
-    # refresh the pair
-    prev = pair[0]
-    pair[0] = pair[1]
-    pair[1] = random.choice(data)
-    while pair[1]['name'] == pair[0]['name'] and pair[1]['name'] == prev['name']:
-      pair[1] = random.choice(data)
+        # refresh the pair
+        prev = pair[0]
+        pair[0] = pair[1]
+        pair[1] = random.choice(data)
 
-  print(
-      f"Compare A: {pair[0]['name']}, {pair[0]['description']}, from {pair[0]['country']}.")
+        while pair[1]['name'] == pair[0]['name'] and pair[1]['name'] == prev['name']:
+            pair[1] = random.choice(data)
 
-  print(vs)
+    print("Compare A:" + printData(pair[0]))
 
-  print(
-      f"Against B: {pair[1]['name']}, {pair[1]['description']}, from {pair[0]['country']}.")
+    print(vs)
 
-  guess = input("Who has more followers? Type 'A' or 'B' (default B): ")
+    print("Against B:" + printData(pair[1]))
 
-  gameContinue, score = calculateAnswer(pair[0], pair[1], guess, score)
+    guess = input("Who has more followers? Type 'A' or 'B' (default B): ")
 
+    gameContinue, score = calculateAnswer(pair[0], pair[1], guess, score)
+
+# out of while loop
+# end of game
 clearConsole()
 print(logo)
 print(f"Sorry, that's wrong. Final score: {score}")
