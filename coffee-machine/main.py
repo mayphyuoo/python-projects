db767d7cbbe28dbe3cefb96c8a259f9eeb677eb9
May Oo <mayphyuoo1211@gmail.com>
2022-03-25 16:32:06 +1100

    initial commit coffee-machine

---
+++ coffee-machine/main.py
@@ -0,0 +1,14 @@
+# Program Requirements
+
+# TODO: 1. Print report.
+# TODO: 2. Check resources sufficient?
+# TODO: 3. Process coins.
+# TODO: 4. Check transaction successful?
+# TODO: 5. Make coffee
+
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
